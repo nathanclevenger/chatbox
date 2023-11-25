b8884794ba40f25cf2d6be3971a34bd8b949517b
Nathan Clevenger <nateclev@gmail.com>
2023-11-24 22:39:23 -0600

    Update api.ts

--- chatbox/components/api.ts
+++ chatbox/components/api.ts
@@ -1,32 +1,36 @@
-import { Redis } from "@upstash/redis";
+import { MongoClient } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function createChatBoxAPI(options: { webhooks: string[] }) {
-  const redis = Redis.fromEnv();
+const client = new MongoClient(process.env.MONGODB_URI);
 
-  return async function (req: NextApiRequest, res: NextApiResponse) {
+type ChatApiOptions = {
+  db: string;
+  collection: string;
+  webhooks: string[];
+};
+
+export default (options: ChatApiOptions) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
     const method = req.method;
-
     const api = req.query.chatbox[0];
     const chatId = req.query.chatbox[1];
     const host = `http://${req.headers.host}`;
 
     try {
       if (!chatId) throw new Error("Missing chatId");
+      const db = client.db(options.db);
+      const collection = db.collection(options.collection);
 
       switch (api) {
         case "chat":
           switch (method) {
-            // GET: /chat/[id]
             case "GET":
-              const chatData = await redis.lrange(chatId, 0, 2 ** 32 - 1);
+              const chatData = await collection.find({ chatId }).toArray();
               return res.status(200).json({ chatData });
 
-            // POST: /chat/[id]
             case "POST":
               const chatText = req.body.text;
-
-              const response = await redis.rpush(chatId, chatText);
+              const response = await collection.insertOne({ chatId, text: chatText });
               return res.status(200).json({ response });
 
             default:
@@ -34,7 +38,6 @@ export default function createChatBoxAPI(options: { webhooks: string[] }) {
           }
 
         case "slack-email":
-          // POST: /slack-email/[id]
           if (method !== "POST") throw new Error("Method not allowed");
 
           const slackEmail = req.body.email;
@@ -42,57 +45,43 @@ export default function createChatBoxAPI(options: { webhooks: string[] }) {
 
           const notifyEmailText = `A user left their email address ${slackEmail} with chat id: ${host}/chat/${chatId}`;
 
-          const requestsEmail = options.webhooks.map(async (webhook) => {
-            return fetch(webhook, {
-              method: "POST",
-              body: JSON.stringify({ text: notifyEmailText }),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-          });
+          const requestsEmail = options.webhooks.map(webhook => fetch(webhook, {
+            method: "POST",
+            body: JSON.stringify({ text: notifyEmailText }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }));
 
           await Promise.all(requestsEmail);
-
           return res.status(200).json({ response: "ok" });
 
         case "slack":
-          // POST: /slack/[id]
           if (method !== "POST") throw new Error("Method not allowed");
 
           let notifyText = `New chat with id: ${host}/chat/${chatId}`;
-
           if (req.body) {
             notifyText = `Old chat with id: ${host}/chat/${chatId} has a new message!`;
           }
 
-          const requestsNotify = options.webhooks.map(async (webhook) => {
-            return fetch(webhook, {
-              method: "POST",
-              body: JSON.stringify({ text: notifyText }),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-          });
+          const requestsNotify = options.webhooks.map(webhook => fetch(webhook, {
+            method: "POST",
+            body: JSON.stringify({ text: notifyText }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }));
 
           await Promise.all(requestsNotify);
-
           return res.status(200).json({ response: "ok" });
 
         default:
           throw new Error("Method not allowed");
       }
     } catch (err) {
-      let message = err;
-
       console.log(err);
-
-      if (err instanceof TypeError) {
-        message = err.message;
-      }
-
+      const message = err instanceof TypeError ? err.message : err;
       return res.status(500).json({ message });
     }
   };
-}
+};
