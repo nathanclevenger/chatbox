b2d11abd1a849d6d1d07ea9590fa031d5b9681f7
Nathan Clevenger <nateclev@gmail.com>
2023-11-29 03:34:10 -0600

    Added db & collection

--- example/pages/api/chatbox/[...chatbox].ts
+++ example/pages/api/chatbox/[...chatbox].ts
@@ -2,6 +2,8 @@ import createChatBoxAPI from "@upstash/chatbox/api";
 
 const ChatBoxAPI = createChatBoxAPI({
   webhooks: [process.env.SLACK_WEBHOOK_URL!],
+  db: 'chatbox',
+  collection: 'chats',
 });
 
 export default ChatBoxAPI;
